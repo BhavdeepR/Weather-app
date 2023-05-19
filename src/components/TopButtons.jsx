a70159a8adc20258071ee352c47c2c374b5ce898
BhavdeepR <bhavdeepr19@gmail.com>
2023-05-19 13:05:03 -0700

    Finish weather app

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 
-function TopButtons()  {
+function TopButtons({setQuery})  {
     const cities = [
         {
             id: 1,
-            title: 'London'
+            title: 'Manteca'
 
         },
         {
             id: 2,
-            title: 'Sydney'
+            title: 'Modesto'
 
         },
         {
             id: 3,
-            title: 'Tokyo'
+            title: 'San Jose'
 
         },
         {
             id: 4,
-            title: 'Toronto'
+            title: 'Stockton'
 
         },
         {
             id: 5,
-            title: 'Paris'
+            title: 'London'
 
         },
         
@@ -34,7 +34,7 @@ function TopButtons()  {
   return (
     <div className = "flex items-center justify-around my=8">
         {cities.map((city) =>(
-            <button key = {city.id} className='text-white text-lg font-medium'>{city.title}</button>
+            <button key = {city.id} className='text-white text-lg font-medium ' onClick={() => setQuery({q: city.title})}>{city.title}</button>
         ))}
     </div>
   )
